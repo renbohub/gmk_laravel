10ab01a1d35217ab25ca8f33f0f9aa83225df614
renbohub <erp.renbo@gmail.com>
2024-04-02 15:10:39 +0700

    color

--- resources/views/main/report.blade.php
+++ resources/views/main/report.blade.php
@@ -218,7 +218,7 @@
             },
             legend: {
                 labels: {
-                    colors: '#fff'
+                    colors: '#000'
                 }
             }
         };
